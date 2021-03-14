5efef3213c0b4ce3f79a62c8c2a5dcb39517e496
IN UH LEE <inuhlee@INui-MacBookPro.local>
2021-03-14 23:35:15 +0900

    순차 요청 -> 병렬 요청

--- src/utils/translate/translate.js
+++ src/utils/translate/translate.js
@@ -8,47 +8,49 @@ const combinePinyins = require('./combinePinyins');
 module.exports = async (req, res) => {
   const { origin, subject } = req.body;
   const sentences = slice(origin);
-  const pinyin = await getPinyin(sentences);
-  if (pinyin.error) {
-    res.status(500).json({
-      error: '네이버로부터 병음을 가져오는데 문제가 생겼습니다.',
+
+  try {
+    const [
+      { value: pinyin },
+      { value: naverTranslation },
+      { value: kakaoTranslation },
+    ] = await Promise.allSettled([
+      getPinyin(sentences),
+      subject == 'kakao' ? [] : Papago(sentences),
+      subject == 'naver' ? [] : Kakao(sentences),
+    ]);
+    const listOfOrigin = combineOrigins(pinyin);
+    const listOfPinyin = combinePinyins(pinyin);
+
+    const data = [];
+    for (let index = 0; index < sentences.length; index++) {
+      data.push({
+        kakao: {
+          text: kakaoTranslation.error
+            ? 'api 요청 한도를 초과하였습니다 ㅠㅠ'
+            : kakaoTranslation[index],
+          modifying: false,
+        },
+        naver: {
+          text: naverTranslation.error
+            ? 'api 요청 한도를 초과하였습니다 ㅠㅠ'
+            : naverTranslation[index],
+          modifying: false,
+        },
+        pinyin: {
+          text: pinyin.error
+            ? '네이버 사전으로부터 병음을 가져오는데 실패하였습니다 ㅠㅠ'
+            : listOfPinyin[index],
+          modifying: false,
+        },
+        origin: listOfOrigin[index],
+      });
+    }
+    return res.status(200).json({
+      data,
     });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
   }
-
-  const listOfOrigin = combineOrigins(pinyin);
-  const listOfPinyin = combinePinyins(pinyin);
-
-  //파파고 api
-  let naverTranslation = subject == 'kakao' ? [] : await Papago(sentences);
-
-  // 카카오 api
-  let kakaoTranslation = subject == 'naver' ? [] : await Kakao(sentences);
-
-  const data = [];
-  for (let index = 0; index < sentences.length; index++) {
-    data.push({
-      kakao: {
-        text: kakaoTranslation.error
-          ? 'api 요청 한도를 초과하였습니다 ㅠㅠ'
-          : kakaoTranslation[index],
-        modifying: false,
-      },
-      naver: {
-        text: naverTranslation.error
-          ? 'api 요청 한도를 초과하였습니다 ㅠㅠ'
-          : naverTranslation[index],
-        modifying: false,
-      },
-      pinyin: {
-        text: pinyin.error
-          ? '네이버 사전으로부터 병음을 가져오는데 실패하였습니다 ㅠㅠ'
-          : listOfPinyin[index],
-        modifying: false,
-      },
-      origin: listOfOrigin[index],
-    });
-  }
-  return res.status(200).json({
-    data,
-  });
 };
